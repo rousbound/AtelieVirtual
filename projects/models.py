306b732018adf29a0d9ad0f0175bc4a5a84929cf
Geraldo Luiz <geraldorj-2010@hotmail.com>
2019-12-23 17:27:16 -0300

    checkpoint, add Cataônibus

--- projects/models.py
+++ projects/models.py
@@ -5,5 +5,6 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    link = models.CharField(max_length=50)
     image = models.FilePathField(path="/img")
     
