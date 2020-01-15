893fb3c56fd8e87f25a47162743795b56437bab1
Geraldo Luiz <geraldorj-2010@hotmail.com>
2020-01-15 11:35:35 -0300

    Changed secret key

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = ')0#vuz+d!2$t*)+tpsg9s527-kp@)b2%7g3ra))o@4%9*&ihtx'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
