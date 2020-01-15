77512a08dbc54fbdb79c81c1e5f6fdf81d3a5269
Geraldo Luiz <geraldorj-2010@hotmail.com>
2020-01-14 22:39:10 -0300

    Commit before checkout to master

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from secretKey import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')0#vuz+d!2$t*)+tpsg9s527-kp@)b2%7g3ra))o@4%9*&ihtx'
+#SECRET_KEY = ')0#vuz+d!2$t*)+tpsg9s527-kp@)b2%7g3ra))o@4%9*&ihtx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
