35c3f06edb41222b627797734f0b31ad8a3c28f3
rousbound <geraldorj-2010@hotmail.com>
2020-03-01 01:21:51 -0300

    Implemented Lectures Image and changed url namespace

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("projects/", include("projects.urls")),
-    path("blog/", include("blog.urls")),
+    path("Lectures/", include("blog.urls")),
+    path("", include("projects.urls")),
     ]
